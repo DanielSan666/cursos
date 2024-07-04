2bd27199f36ce663137003642eb913a348426a2d
Junior Castañeda Rios <bc4363624@gmail.com>
2024-07-03 22:12:10 -0600

    v2

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import login from './src/pages/login';
-import home from './src/pages/home';
-import MenuPage from './src/pages/menu';
-import ProfilePage from './src/pages/profile';
-import SettingsPage from './src/pages/settings';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import login from "./src/pages/login";
+import home from "./src/pages/home";
+import MenuPage from "./src/pages/menu";
+import ProfilePage from "./src/pages/profile";
+import SettingsPage from "./src/pages/settings";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,12 +13,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="home">
-        <Stack.Screen name="home" component={home} />
+        <Stack.Screen name="Home" component={home} />
         <Stack.Screen name="Login" component={login} />
         <Stack.Screen name="Menu" component={MenuPage} />
-        <Stack.Screen name='profile' component={ProfilePage} />
-        <Stack.Screen name='settings' component={SettingsPage} />
-
+        <Stack.Screen name="profile" component={ProfilePage} />
+        <Stack.Screen name="settings" component={SettingsPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
